c4503c6f38a54445d322d2c0e619cbb2fa2c1a2a
jlorbor2710 <lorite.borrego.jose.tomas@iescamas.es>
2025-11-25 22:35:49 +0100

    Feat: Add second activity for sound and video modes
    Adds a new activity, `MainActivity2`, to handle different relaxation modes. Implements `OnClickListener` for the sound and video buttons on the main screen to launch this new activity, passing the selected mode ("Sonido" or "Video") via an Intent.

--- Centroderelajacion/app/src/main/java/com/example/centroderelajacin/MainActivity.java
+++ Centroderelajacion/app/src/main/java/com/example/centroderelajacin/MainActivity.java
@@ -22,7 +22,25 @@ public class MainActivity extends AppCompatActivity {
         buttonSonido = findViewById(R.id.buttonSonido);
         buttonVideo = findViewById(R.id.buttonVideo);
 
+        buttonSonido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                String modo = "Sonido";
+                intent.putExtra("modo", modo);
+                startActivity(intent);
 
+            }
+        });
+        buttonVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                String modo = "Video";
+                intent.putExtra("modo", modo);
+                startActivity(intent);
+            }
+        });
 
 
 
