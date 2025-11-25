47f8b1356e8bd0d45af7c93d47e7e306f829b074
jlorbor2710 <lorite.borrego.jose.tomas@iescamas.es>
2025-11-25 03:49:53 +0100

    Centro de Relajacion añadido

---
+++ Centroderelajacion/app/src/main/java/com/example/centroderelajacin/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.centroderelajacin;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonSonido;
+    Button buttonVideo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSonido = findViewById(R.id.buttonSonido);
+        buttonVideo = findViewById(R.id.buttonVideo);
+
+
+
+
+
+    }
+}
